1d4eb58b25dc643774cca5da89e670ebbe3b941c
masouddaloji <masouddaloji@gmail.com>
2023-07-03 14:50:24 +0430

    try to fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AOS from 'aos';
 //style aos
 import 'aos/dist/aos.css';
 //components
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Layout from "./components/Layout/Layout";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
