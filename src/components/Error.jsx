cca2c3dd4f95e20e1a9ced0bc1eb0a88c92fbee7
masouddaloji <masouddaloji@gmail.com>
2023-07-11 15:58:15 +0430

    fixed width for message box

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,9 +3,9 @@ import {ImWarning} from 'react-icons/im'
 
 const Error = () => {
   return (
-    <section className='w-1/2 text-sm flex items-center gap-x-2.5 mx-auto my-2.5 dark:bg-white bg-silver  text-primary rounded-xl px-10 py-2 '>
+    <section className='w-full md:w-1/2 text-sm flex items-center gap-x-2.5 mx-auto my-2.5 dark:bg-white bg-silver  text-primary rounded-xl px-10 py-2 '>
     <ImWarning className=' text-xl  '/>
-        <p className=" ">موردی برای این دسته بندی یافت نشد</p>
+        <p className=" ">موردی یافت نشد</p>
     </section>
   )
 }
